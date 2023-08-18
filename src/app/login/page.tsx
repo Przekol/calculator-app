b227421e329a06f1be313e5e0013653009fa1723
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-08-18 14:56:26 +0200

    refactor: Updated LoginPage by using LoginButton component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,51 +1,40 @@
-'use client';
-import { Button, Container, Paper, Typography } from '@mui/material';
-import GoogleIcon from '@mui/icons-material/Google';
+import React from 'react';
+import { Container, Paper, Typography } from '@mui/material';
 import CalculateIcon from '@mui/icons-material/Calculate';
 
-export default function LoginPage() {
-  function handleSignInWithGoogle(): void {
-    console.log('Zalogowany');
-  }
+import AuthGuardRouteRedirect from '@/app/components/PrivateRoute/AuthGuardRouteRedirect';
+import LoginButton from '@/app/components/buttons/LoginButton';
 
+export default function LoginPage() {
   return (
-    <Container
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '80vh',
-      }}
-    >
-      <Paper
-        elevation={6}
+    <>
+      <AuthGuardRouteRedirect />
+      <Container
         sx={{
           display: 'flex',
-          flexDirection: 'column',
+          justifyContent: 'center',
           alignItems: 'center',
-          gap: 4,
-          padding: 6,
-          borderRadius: 5,
+          height: '80vh',
         }}
       >
-        <CalculateIcon sx={{ fontSize: 100, color: 'primary.main' }} />
-        <Typography variant={'h4'} align="center" gutterBottom={true}>
-          Log in to Calculator App
-        </Typography>
-        <Button
-          variant={'outlined'}
+        <Paper
+          elevation={6}
           sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 4,
+            padding: 6,
             borderRadius: 5,
-            width: '100%',
-            fontSize: { xs: '16px', sm: '24px' },
-            textTransform: 'none',
           }}
-          onClick={handleSignInWithGoogle}
         >
-          <GoogleIcon sx={{ fontSize: { xs: '20px', sm: '28px' }, marginRight: 2 }} />
-          Log in with Google
-        </Button>
-      </Paper>
-    </Container>
+          <CalculateIcon sx={{ fontSize: 100, color: 'primary.main' }} />
+          <Typography variant={'h4'} align="center" gutterBottom={true}>
+            Log in to Calculator App
+          </Typography>
+          <LoginButton />
+        </Paper>
+      </Container>
+    </>
   );
 }
