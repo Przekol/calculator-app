449025e13c7652bdb4391aced93cff5d7d1b35a8
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-08-18 21:46:55 +0200

    refactor: Updated DashboardPage content

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,30 +1,10 @@
-'use client';
-
-import { Button } from '@mui/material';
-import GoogleIcon from '@mui/icons-material/Google';
 import React from 'react';
-import { useAuthUser } from '@/app/hooks/useAuthUser';
-import { useAuthActions } from '@/app/hooks/useAuthActions';
+import Box from '@mui/material/Box';
 
 export default function DashboardPage() {
-  const { user, loading } = useAuthUser();
-  const { signOut } = useAuthActions();
   return (
-    <main>
-      Home Page {user?.email}
-      <Button
-        variant={'outlined'}
-        sx={{
-          borderRadius: 5,
-          width: '100%',
-          fontSize: { xs: '16px', sm: '24px' },
-          textTransform: 'none',
-        }}
-        onClick={e => signOut()}
-      >
-        <GoogleIcon sx={{ fontSize: { xs: '20px', sm: '28px' }, marginRight: 2 }} />
-        Log Out
-      </Button>
-    </main>
+    <Box>
+      <h1>Home Page</h1>
+    </Box>
   );
 }
