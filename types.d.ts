305fd67f00c961c99a167bfc3c1f710d503ad4ac
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-08-20 18:43:10 +0200

    feat: Created getLatestCalculations function for fetching calculation data

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Operation } from '@/app/components/buttons/OperationButton';
 import { AlertColor } from '@mui/material/Alert/Alert';
+import { Timestamp } from '@firebase/firestore';
 
 type NavItem = {
   title: string;
@@ -20,4 +21,14 @@ type OperationData = Omit<CalculationData, 'result'>;
 type AlertData = {
   message: string;
   severity: AlertColor;
+};
+
+type CalculationResult = CalculationData & {
+  id: string;
+  time: string;
+  timestamp: Timestamp;
+};
+
+type GetCalculationsOptions = {
+  limit?: number;
 };
